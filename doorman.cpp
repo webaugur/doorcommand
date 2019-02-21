d82d8a68a1b853c0289fe0b5cba0cda9ee0610d7
Augur <dave@webaugur.com>
2019-02-20 21:02:25 -0500

    DoorCommand API Begins
    Fleshing out the beginning of a DoorCommand Singleton with method chaining for config.

--- doorman.cpp
+++ doorman.cpp
@@ -1,23 +1,217 @@
-// This is the main project file for VC++ application project 
-// generated using an Application Wizard.
+/* DoorCommand - Command Line Interface for Door Access Controllers
+ * 
+ * Copyright 2019 David L Norris <dave@webaugur.com>
+ *
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list 
+ * of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this 
+ * list of conditions and the following disclaimer in the documentation and/or other 
+ * materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ * OF SUCH DAMAGE.
+ *
+ */
 
 #include "stdafx.h"
 #include <windows.h>
+#include <vcclr.h>
 #include <string>
 #include <iostream>
-#using <mscorlib.dll>
-
-using namespace System;
-
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "tchar.h"
 #include "getopt.h"
 
+using namespace System;
+
+/* I do declare */
+void BecauseIUsedToBeOne(void);
+
+/*!
+	\brief Door Command Object
+
+	Implemenation of the Door Command API.
+*/
+__gc class DoorCommand
+{
+public:
+	/*!< /brief DoorCommand constructor
+		
+		DoorCommand is arranged as a SingleTon pattern. 
+		Method chaining is available for many operations.
+	 */
+	DoorCommand(){
+		// new object needs to be initialized
+		if(!_instance){
+			_connection = new WComm_UDP::WComm_Operate();
+			_serial = 0;
+			_command = "";
+			_result = "";
+			_ip_port = 60000;
+			_ip_address = ""; // If IP Address is Empty String Broadcast Mode is enabled
+			_status = 0;
+			_swipe_date = "";
+			_instance = this;
+		} 
+		// existing object just needs to be returned
+		else {
+			 _instance = this;
+		}
+	}
+
+	/*!
+		\brief Set Serial Number of Door
+
+		This serial number is used to communicate with the desired door controller.
+		This is literally the only access token required. You don't even need to know
+		the IP address of the device. This function is not likely to be called publicly
+		but there is no reason, currently, not to do so.
+	*/
+	DoorCommand* setSerial(long serial /**< [in] Door Controller Serial Number. */)
+    {
+		_serial = serial;
+        return _instance; /**< [out] instance of self for method chaining. */
+    }
+
+	/*!
+		\brief Get Serial Number of Door
+
+		The serial number used communicate with the desired door controller.
+	*/
+	long getSerial()
+	{
+		return _serial; /**< [out] Door Controller Serial Number. */
+	}
+
+	/*!
+		\brief Next Queued Command
+
+		Holds the next Command to be issued. 
+		This is used mostly internally by specific methods.  
+		Returns Empty String if there is no command to be executed.
+	*/
+	DoorCommand* setCommand(String* command/**< [in] Text of Command. */)
+    {
+		_command = command;
+        return _instance; /**< [out] instance of self for method chaining. */
+    }
+
+	/*!
+		\brief Queued Command
+
+		Contains the text of the queued command. Mostly used internally by
+		other methods.
+	*/
+	String* getCommand()
+	{
+		return _command; /**< [out] Command to be executed (may be ""). */
+	}
 
 
-int main (int argc, char **argv)
+	/*!
+		\brief Most Recent Result
+
+		Holds the results of the most recent command
+		This is used mostly internally by specific methods.  
+		Returns Empty String if there is no result.
+	*/
+	DoorCommand* setResult(String* result/**< [in] Text of Result. */)
+    {
+		_result = result;
+        return _instance; /**< [out] instance of self for method chaining. */
+    }
+
+	/*!
+		\brief Result from most recent Command
+
+		Contains the text of the queued command. Mostly used internally by
+		other methods.
+	*/
+	String* getResult()
+	{
+		return _result; /**< [out] Result of the most recent command. */
+	}
+
+
+	/*!
+		\brief set IP Address
+
+		Holds the IP Address.
+		If Empty String Broadcast Mode is Used
+	*/
+	DoorCommand* setIPAddress(String* ip_address/**< [in] Text of IPv4 Address, Empty = Broadcast. */)
+    {
+		_ip_address = ip_address;
+        return _instance; /**< [out] instance of self for method chaining. */
+    }
+
+	/*!
+		\brief get IP Address
+
+		Contains the text of the IPv4 address of this door.
+	*/
+	String* getIPAddress()
+	{
+		return _ip_address; /**< [out] IP Address (may be "" for broadcast). */
+	}
+
+	/*!
+		\brief set IP Port
+
+		Holds the IP Port. Must be an integer 1024-65535
+	*/
+	DoorCommand* setIPPort(int ip_port/**< [in] Text of IPv4 Port. */)
+    {
+		// bounds check the port number
+		if( (ip_port > 1024) && (ip_port <= 65535) ){
+			_ip_port = ip_port;
+			return _instance; /**< [out] instance of self for method chaining. */
+		}
+		else {
+			throw std::invalid_argument( "IP Port must be 1024-65535" );
+		}
+    }
+
+	/*!
+		\brief get IP Port
+
+		Contains the IPv4 port number for this door.
+	*/
+	int getIPPort()
+	{
+		return _ip_port; /**< [out] IP Port, defaults to 60000. */
+	}
+
+
+
+private:
+	static DoorCommand			*_instance	= NULL;	//!< instance of _self
+	WComm_UDP::IWCOMM_OPERATE	*_connection;	//!< .NET UDP object
+	long						_serial;		//!< controllerSN - Serial Number of the Door to Command
+	String*						_command;		//!< strCmd - Command, Serial Data Frame
+	String*						_result;		//!< strFrame - Result of Command, Serial Data Frame
+    int							_ip_port;		//!< ipPort - Controller TCP Port
+    String*						_ip_address;	//!< ipAddr - Controller IP Address
+    __int64						_status;        //!< status - Status     
+    String*						_swipe_date;	//!< strSwipeDate - Date time
+};
+
+
+int _tmain (int argc, char **argv)
 {
   int aflag = 0;
   int bflag = 0;
@@ -60,6 +254,11 @@ int main (int argc, char **argv)
     printf ("Non-option argument %s\n", argv[index]);
 
 
+	/* Still doing all the things we used to do */
+	BecauseIUsedToBeOne();
+
+	return 0;
+}
 
 
 
@@ -67,16 +266,8 @@ int main (int argc, char **argv)
 
 
 
-
-
-
-
-
-    // TODO: Please replace the sample code below with your own.
-    // Console::WriteLine(S"Hello World");
-    // Console::WriteLine("Current code page: {0}\n", 
-    //                    Console.OutputEncoding.CodePage);
-
+/* I don't do main() because I used to be one */
+  void BecauseIUsedToBeOne(void){
 	WComm_UDP::IWCOMM_OPERATE		*wudp;
     wudp = new WComm_UDP::WComm_Operate();
 
@@ -202,5 +393,4 @@ int main (int argc, char **argv)
 	}
     
     wudp = NULL;
-	return 0;
 }
