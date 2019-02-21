7a62908482df2767f343b37ff063d6d6f4e65a96
Augur <dave@webaugur.com>
2019-02-21 02:02:58 -0500

    Removed Test Code
    No longer need this pile of garbage. Out with the old and in with the new.

--- doorman.h
+++ doorman.h
@@ -3,7 +3,6 @@
 #include "doorcommand.h"
 
 /* I do declare */
-void BecauseIUsedToBeOne(void);
 void printValidCardCount(DoorCommand* command);
 void printDateTime(DoorCommand* command);
 void openDoors(DoorCommand* command, int door_numbers);
